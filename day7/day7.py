c652a8a3ad1b5ac6c396742811c1d45c211ba8f3
Stefan Rozmanowski <Stefan.Rozmanowski@bglgroup.co.uk>
2021-12-07 10:58:16 +0000

    fix day 7 to generalise

--- day7/day7.py
+++ day7/day7.py
@@ -7,12 +7,17 @@ def triangular(n):
     return n*(n+1)/2
 
 def part2(input):
-    return np.sum(triangular(np.abs(np.array(input) - np.floor(np.mean(input)))))
+    round_up = np.sum(triangular(np.abs(np.array(input) - np.floor(np.mean(input)))))
+    round_down = np.sum(triangular(np.abs(np.array(input) - np.ceil(np.mean(input)))))
+    return min([round_up, round_down])
 
 if __name__=="__main__":
     with open("day7/inputs/input1.txt", "r") as f:
         input1 = [int (n) for n in f.readline().split(",")]
+    with open("day7/inputs/test1.txt", "r") as f:
+        test1 = [int (n) for n in f.readline().split(",")]
 
     print(part1(input1))
+    print(part2(test1))
     print(part2(input1))
     
